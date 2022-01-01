64e4730157d41b91216f6a842857691127703640
PitFiend13 <89072628+PitFiend13@users.noreply.github.com>
2022-01-01 19:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ Created on Thu Dec 30 12:03:08 2021
 """
 from flask import Flask,render_template,request
 import pickle
-import numpy as np
+# import numpy as np
 app= Flask(__name__)
-model=pickle.load(open('model.pkl','rb'))
+model=pickle.load(open('musicrecommendormodel.pkl','rb'))
 @app.route('/',methods='GET')
 def webpage():
     return render_template('mlmodel.html')
-@app.route('/predict',methods=['POST'])
+@app.route("/predict",methods=['POST'])
 def predictions():
     age=request.form['age']
     gender=request.form['gender']
