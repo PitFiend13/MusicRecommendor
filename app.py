6a0b7354583f30c0f34b2383c32014618655d9d4
PitFiend13 <89072628+PitFiend13@users.noreply.github.com>
2022-01-01 19:32:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 30 12:03:08 2021
+
+@author: arnav
+"""
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+app= Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+@app.route('/',methods='GET')
+def webpage():
+    return render_template('mlmodel.html')
+@app.route('/predict',methods=['POST'])
+def predictions():
+    age=request.form['age']
+    gender=request.form['gender']
+    prediction=model.predict([[age,gender]])
+    output=prediction[0]
+    return render_template('mlmodel.html',pred=output)
+if __name__=='__main__':
+    app.run(debug=True)
